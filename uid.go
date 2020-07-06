103278ef7b1c89184a8b8ae713de2088aa264d13
Milan Lesichkov <lesichkovm@gmail.com>
2020-07-06 04:50:20 +0100

    Create uid.go

---
+++ uid.go
@@ -0,0 +1,39 @@
+package uid
+
+import (
+	"crypto/rand"
+	"strings"
+	"time"
+)
+
+func HimanUid() string {
+	r, _ := rand.Prime(rand.Reader, 64)
+	id := time.Now().UTC().Format("20060102150405.0000000")
+	id = strings.ReplaceAll(id, ".", "")
+	id += r.String()
+	return id[0:32]
+}
+
+func NanoUid() string {
+	r, _ := rand.Prime(rand.Reader, 64)
+	id := time.Now().UTC().Format("20060102150405.0000000")
+	id = strings.ReplaceAll(id, ".", "")
+	id += r.String()
+	return id[0:23]
+}
+
+func MicroUid() string {
+	r, _ := rand.Prime(rand.Reader, 64)
+	id := time.Now().UTC().Format("20060102150405.0000000")
+	id = strings.ReplaceAll(id, ".", "")
+	id += r.String()
+	return id[0:20]
+}
+
+func SecUid() string {
+	r, _ := rand.Prime(rand.Reader, 64)
+	id := time.Now().UTC().Format("20060102150405.0000000")
+	id = strings.ReplaceAll(id, ".", "")
+	id += r.String()
+	return id[0:23]
+}
